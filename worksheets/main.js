ec812ffbafbdca34f7aab8e0fd8d435823916dd3
3omni <noticiasmundiales99@gmail.com>
2019-02-02 16:24:22 +0100

    changes

--- worksheets/main.js
+++ worksheets/main.js
@@ -1,32 +1,33 @@
 window.onload = function(){
 	var menu_item = document.getElementsByClassName("button_menu");
+	/*
 	menu_item[1].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '100px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 242, 204)";
 	};
 	menu_item[2].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '100px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 		document.getElementById("list_menu").style.backgroundColor = "rgb(204, 255, 255)";
 	};
 	menu_item[3].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '100px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 204, 230)";
 	};
 	menu_item[4].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '100px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 		document.getElementById("list_menu").style.backgroundColor = "rgb(217, 255, 204)";
 	};
 	menu_item[0].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 	};
 	menu_item[5].onmouseover = function() {
-		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("list_menu").style.transform = "translateY(50px)";
 	};
 	document.getElementById("list_menu").onmouseout = function() {
-		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("list_menu").style.transform = "translateY(-50px)";
 	};
 	document.getElementById("header").onmouseover = function() {
-		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("list_menu").style.transform = "translateY(-50px)";
 	};
 
 
@@ -39,4 +40,5 @@ window.onload = function(){
 		//menu_item[0].classList.add("ini_button_class");
 		//document.getElementById("ini_icon").style.opacity = '1';
 	//};
+	*/
 }
