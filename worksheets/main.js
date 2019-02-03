074eef288977ba786ff894b83ec73aa951d840c5
3omni <noticiasmundiales99@gmail.com>
2019-02-03 12:15:29 +0100

    changes

--- worksheets/main.js
+++ worksheets/main.js
@@ -1,44 +1,34 @@
 window.onload = function(){
 	var menu_item = document.getElementsByClassName("button_menu");
-	/*
 	menu_item[1].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 242, 204)";
+		//document.getElementById("submenu_bar").style.top = '100px';
 	};
 	menu_item[2].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(204, 255, 255)";
 	};
 	menu_item[3].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 204, 230)";
 	};
 	menu_item[4].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(217, 255, 204)";
 	};
 	menu_item[0].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '0px';
 	};
 	menu_item[5].onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(50px)";
+		document.getElementById("list_menu").style.height = '0px';
 	};
 	document.getElementById("list_menu").onmouseout = function() {
-		document.getElementById("list_menu").style.transform = "translateY(-50px)";
+		document.getElementById("list_menu").style.height = '0px';
 	};
 	document.getElementById("header").onmouseover = function() {
-		document.getElementById("list_menu").style.transform = "translateY(-50px)";
+		document.getElementById("list_menu").style.height = '0px';
 	};
 
 
-
-
-
-	//menu_item[0].onmouseover = function() {
-		//menu_item[0].classList.add("button_menu_hover");
-		//menu_item[0].classList.add("button_text_hover");
-		//menu_item[0].classList.add("ini_button_class");
-		//document.getElementById("ini_icon").style.opacity = '1';
-	//};
-	*/
 }
