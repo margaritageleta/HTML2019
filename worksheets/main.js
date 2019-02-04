84317cc0051bc1ba4afc7d897f90650588cf8dfa
3omni <noticiasmundiales99@gmail.com>
2019-02-04 10:11:21 +0100

    changes

--- worksheets/main.js
+++ worksheets/main.js
@@ -3,32 +3,41 @@ window.onload = function(){
 	menu_item[1].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 242, 204)";
-		//document.getElementById("submenu_bar").style.top = '100px';
+		document.getElementById("submenu_bar").style.top = '144px';
 	};
 	menu_item[2].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(204, 255, 255)";
+		document.getElementById("submenu_bar").style.top = '144px';
 	};
 	menu_item[3].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(255, 204, 230)";
+		document.getElementById("submenu_bar").style.top = '144px';
 	};
 	menu_item[4].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '100px';
 		document.getElementById("list_menu").style.backgroundColor = "rgb(217, 255, 204)";
+		document.getElementById("submenu_bar").style.top = '144px';
 	};
 	menu_item[0].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("submenu_bar").style.top = '-144px';
+
 	};
 	menu_item[5].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '0px';
-	};
-	document.getElementById("list_menu").onmouseout = function() {
-		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("submenu_bar").style.top = '-144px';
+
 	};
 	document.getElementById("header").onmouseover = function() {
 		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("submenu_bar").style.top = '-144px';
+
 	};
+	document.getElementById("sec").onmouseover = function() {
+		document.getElementById("list_menu").style.height = '0px';
+		document.getElementById("submenu_bar").style.top = '-144px';
 
-
+	};
 }
