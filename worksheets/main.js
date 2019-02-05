6dc64b2d581b31994f5d4305d0aae91aa7d4fe2d
3omni <noticiasmundiales99@gmail.com>
2019-02-05 08:06:20 +0100

    changes

--- worksheets/main.js
+++ worksheets/main.js
@@ -120,4 +120,45 @@ window.onload = function(){
 		document.getElementById("contact_list_menu").style.display = "none";
 
 	};
+	var not_menu_elem = document.getElementsByClassName("not_menu");
+	//???
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+	showSlidesAuto();
+
+		window.plusSlides = plusSlides; //make it global
+		// Next/previous controls
+		function plusSlides(n) {
+  			showSlides(slideIndex += n);
+		}
+
+		window.currentSlide = currentSlide;
+		// Thumbnail image controls
+		function currentSlide(n) {
+  			showSlides(slideIndex = n);
+		}
+
+		function showSlides(n) {
+  			var i;
+  			var slides = document.getElementsByClassName("slide");
+  			if (n > slides.length) {slideIndex = 1} 
+  			if (n < 1) {slideIndex = slides.length}
+  			for (i = 0; i < slides.length; i++) {
+  		    	slides[i].style.display = "none"; 
+  		    }
+  			slides[slideIndex-1].style.display = "block"; 
+
+  		}
+  		function showSlidesAuto() {
+  			var i;
+  			var slides = document.getElementsByClassName("slide");
+  			for (i = 0; i < slides.length; i++) {
+    			slides[i].style.display = "none"; 
+  			}
+  			slideIndex++;
+  			if (slideIndex > slides.length) {slideIndex = 1} 
+  			slides[slideIndex-1].style.display = "block"; 
+  			setTimeout(showSlidesAuto, 5000); // Change image every 2 seconds
+		}
 }
