06fb3f2fad68b6136125bbd923eebebbfc7cba2f
3omni <noticiasmundiales99@gmail.com>
2019-02-05 12:43:18 +0100

    changes working !!

--- worksheets/main.js
+++ worksheets/main.js
@@ -63,7 +63,7 @@ window.onload = function(){
 	};
 	menu_item[0].onmouseover = function() {
 		document.getElementById("list_menu").style.height = '0px';
-		document.getElementById("submenu_bar").style.top = '-43px';
+		document.getElementById("submenu_bar").style.top = '43px';
 		///*
 		document.getElementById("prod_list_menu").style.opacity = '0';
 		document.getElementById("brand_list_menu").style.opacity = '0';
@@ -105,7 +105,7 @@ window.onload = function(){
 		document.getElementById("contact_list_menu").style.display = "none";
 
 	};
-	document.getElementById("sec").onmouseover = function() {
+	document.getElementById("main").onmouseover = function() {
 		document.getElementById("list_menu").style.height = '0px';
 		document.getElementById("submenu_bar").style.top = '43px';
 		///*
@@ -120,45 +120,5 @@ window.onload = function(){
 		document.getElementById("contact_list_menu").style.display = "none";
 
 	};
-	var not_menu_elem = document.getElementsByClassName("not_menu");
-	//???
-
-	var slideIndex = 1;
-	showSlides(slideIndex);
-	showSlidesAuto();
-
-		window.plusSlides = plusSlides; //make it global
-		// Next/previous controls
-		function plusSlides(n) {
-  			showSlides(slideIndex += n);
-		}
-
-		window.currentSlide = currentSlide;
-		// Thumbnail image controls
-		function currentSlide(n) {
-  			showSlides(slideIndex = n);
-		}
-
-		function showSlides(n) {
-  			var i;
-  			var slides = document.getElementsByClassName("slide");
-  			if (n > slides.length) {slideIndex = 1} 
-  			if (n < 1) {slideIndex = slides.length}
-  			for (i = 0; i < slides.length; i++) {
-  		    	slides[i].style.display = "none"; 
-  		    }
-  			slides[slideIndex-1].style.display = "block"; 
-
-  		}
-  		function showSlidesAuto() {
-  			var i;
-  			var slides = document.getElementsByClassName("slide");
-  			for (i = 0; i < slides.length; i++) {
-    			slides[i].style.display = "none"; 
-  			}
-  			slideIndex++;
-  			if (slideIndex > slides.length) {slideIndex = 1} 
-  			slides[slideIndex-1].style.display = "block"; 
-  			setTimeout(showSlidesAuto, 5000); // Change image every 2 seconds
-		}
+	
 }
