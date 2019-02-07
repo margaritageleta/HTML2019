c042ba1cdbdf3893a388bd66ed33c2c691d0d5c2
3omni <noticiasmundiales99@gmail.com>
2019-02-07 13:57:04 +0100

    changes

--- worksheets/poke.js
+++ worksheets/poke.js
@@ -1,63 +1,85 @@
-var id = 1;
+var val = 1;
 
-function get_search_data(val) {
-	console.log(val);
+// input=0 arrows, input=1 button.
+function get_url(val, input) {
 	var url;
-	if($.isNumeric(val)) url = "http://51.144.236.83:8080/pokemons?num=";
-	else url = "http://51.144.236.83:8080/pokemons?name=";
-	$.get(url + val, function(data){
-		id = data[0].id;
-		get_data(id);
-	}, "json");
+	if($.isNumeric(val) && input == 1) { 
+		url = "http://51.144.236.83:8080/pokemons?num=";
+		$.get(url + val, function(data){
+			data = data[0];
+			get_data(data);
+		}, "json");
+	}
+	else if (typeof val == 'string' && input == 1) { 
+		url = "http://51.144.236.83:8080/pokemons?name=";
+		$.get(url + val, function(data){
+			data = data[0];
+			get_data(data);
+		}, "json");
+	}
+	else{ 
+		url = "http://51.144.236.83:8080/pokemons/";
+		$.get(url + val, function(data){
+			get_data(data);
+		}, "json")
+		.fail(function(){
+			get_data("");
+		});
+	}
 }
 
-function get_data(id) {
-	console.log(id);
+
+
+
+function get_data(data) {
+	console.log(data);
 	$("li").remove();
-	$.get("http://51.144.236.83:8080/pokemons/" + id, function(data){
-		if(data == ""){
-			$("#code").text("no data");
-			$("#name").text("no data");
-			$("#HM").text("no data");
-		}
-		else{
-			$("#code").text(data.num);
-			$("#normal_sprite").attr('src',data.images[0].front);
-			$("#shiny_sprite").attr('src',data.images[1].front_shiny);
-			$("#name").text(data.name);
-			data.types.forEach(function(elem){
-				$("#lista").append("<li>" + elem.type.name + "</li>");
-			});
-			if(data.moves.HM == null) $("#HM").text("null");
-			else $("#HM").text(data.moves.HM);	
-		}
-	}, "json");
+	if(data == ""){
+		$("#code").text("no data");
+		$("#normal_sprite").attr('src',"");
+		$("#shiny_sprite").attr('src',"");
+		$("#name").text("no data");
+		$("#HM").text("no data");
+		$("#lista").append("<li>" + "no data" + "</li>");
+	}
+	else{
+		$("#code").text(data.num);
+		$("#normal_sprite").attr('src',data.images[0].front);
+		$("#shiny_sprite").attr('src',data.images[1].front_shiny);
+		$("#name").text(data.name);
+		data.types.forEach(function(elem){
+			$("#lista").append("<li>" + elem.type.name + "</li>");
+		});
+		if(data.moves.HM == null) $("#HM").text("null");
+		else $("#HM").text(data.moves.HM);	
+		val = data.id;
+	}
 }
 
 function check(){
-	if(id > 6) id = 1;
-	else if (id < 1) id = 6;
+	if(val > 6) val = 1;
+	else if (val < 1) val = 6;
 }
 
 
 
 $().ready(function(){ // window.onload....
-	get_data(id);
+	get_url(val, 0);
 	$("#next").click(function(){
 		console.log("click next");
-		id = id + 1;
+		val = val + 1;
 		check();
-		get_data(id);
+		get_url(val, 0);
 	});
 	$("#prev").click(function(){
 		console.log("click prev");
-		id = id - 1;
+		val = val - 1;
 		check();
-		get_data(id);
+		get_url(val, 0);
 	});
 	$("#btn").click(function(){
-    	var get_val = $("#input_bar").val();
-   		console.log(get_val);
-   		get_search_data(get_val);
+    	var val = $("#input_bar").val();
+   		console.log(val);
+   		get_url(val, 1);
   	});
 });
